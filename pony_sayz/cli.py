6d4729e09fb19ae71c0677cca219c1f0c41fb53e
Paulo P <pssp25@hotmail.com>
2020-11-06 19:25:54 +0100

    Improved the parsing.

--- pony_sayz/cli.py
+++ pony_sayz/cli.py
@@ -9,11 +9,27 @@ def main():
     parser.add_argument('_', nargs='*')
     args = parser.parse_args()
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "pony_sayz.cli.main")
+    #print("Arguments: " + str(args._))
+    #print("Replace this message by putting your code into "
+    #      "pony_sayz.cli.main")
+    msg = args._[0]
+    nr_traces = len(msg) + 4
+    traces = '-' * nr_traces
 
-    
+    filepath = './data/ace.pony'
+    line_nr = 1
+    with open(filepath, encoding='utf-8') as fp:
+        lines = fp.readlines()
+        for line in lines:
+            if line_nr > 21:
+                if ('balloon5' in line):
+                    print(traces)
+                    print(line.replace('balloon5',args._[0]).replace('$',''))
+                    print(traces)
+                else:
+                    print(line.replace('\n','').replace('$','')) 
+            line_nr += 1
+
     return 0
 
 
